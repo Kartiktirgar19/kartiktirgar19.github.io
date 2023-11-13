3d3f0f86f9cb758e5c378d0ca2f6e5819aa29901
Kartiktirgar19 <kartik.tirgar19@gmail.com>
2023-11-13 21:48:38 +0530

    Adding css and js file

---
+++ app.js
@@ -0,0 +1,23 @@
+const computerChoices = ['bat', 'ball', 'stump'];
+const resultMsgs = [ ` tie`, `user won .`, `computer won`];
+
+function getRandomNumber(){
+  let rendomnumber = Math.random() * 3;
+  if( rendomnumber >0 && rendomnumber <= 1 ){
+    return 0;
+  } 
+  else if ( rendomnumber > 1 && rendomnumber <= 2 ){
+    return 1;
+  }
+  else{
+    return 2;
+  }
+}
+
+function playGame( userChoice ){
+  const computerChoiceIndex = getRandomNumber();
+  let computerchoice = computerChoices[computerChoiceIndex];
+  let resultmeg = resultMsgs[computerChoiceIndex];
+  document.getElementById('result').innerHTML = 
+`you have chosen ${userChoice}. computer choice is ${computerchoice} ${resultmeg}`;
+}
